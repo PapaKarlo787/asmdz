834e78a809c85bee06d259e4f7a655ba09466c5b
PapaKarlo787 <33232815+PapaKarlo787@users.noreply.github.com>
2021-03-13 16:44:30 +0300

    Update os.sh

--- os.sh
+++ os.sh
@@ -3,9 +3,6 @@ mkdir bin 2>/dev/null
 
 for f in `ls progs`; do
 	echo $f
-	fasm progs/$f/$f.asm bin/$f
-#	echo tasm progs\\$f\\$f.asm bin\\$f >> exec.bat#
-#	echo tlink bin\\$f.obj /x /t /3 >> exec.bat
+	fasm progs/$f/$f.asm bin/$f 2> /dev/null
 done
-#echo exit >> exec.bat
 python3 os.py <<< `ls bin`
